8c45af24e02aa8f599df1c717c6f672847d4fb4f
Pedro Skakum <pedroskakum@gmail.com>
2026-06-12 20:29:05 -0300

    feat: comment

--- full-cycle/9 - worker/main.go
+++ full-cycle/9 - worker/main.go
@@ -15,6 +15,13 @@ func worker(workerId int, msg chan int) {
 
 func main() {
 	msg := make(chan int)
+
+	/*
+		quanto mais workers eu criar, mais rapido o processamento da fila vai acontecer,
+		pois cada worker vai processar uma parte da fila, de forma independente,
+		e isso e uma das vantagens de usar go routines, pois elas permitem que voce crie
+		muitas threads de forma facil, e isso e muito util para processar filas de tarefas
+	*/
 	go worker(1, msg) // go routine
 	go worker(2, msg) // go routine
 
